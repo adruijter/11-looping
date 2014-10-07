720a96562dea9d1627362522440fe9db4390147e
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-07 10:41:37 +0200

    do while toegevoegd

---
+++ do_while/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Voorbeeld van een do.. while lus
+             */
+            ConsoleKeyInfo cki;
+            Console.WriteLine("Voer een toetsenbord letter in!");
+            Console.WriteLine("+++++++++++++++++++++++++++++++");
+            do
+            {
+                Console.WriteLine("\nU heeft niet de goede toets ingedrukt. \nProbeer nog eens!");
+                Console.Write("Nieuwe letter: ");
+                cki = Console.ReadKey();
+                Console.WriteLine("\n++++++++++++++++++++++++++++++++");
+                
+
+            } while( cki.KeyChar != (char)ConsoleKey.H);
+            Console.WriteLine("\nGefeliciteerd! \nU heeft de goede letter ingevoerd");
+            Console.Write("De letter was: {0}", cki.KeyChar.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
