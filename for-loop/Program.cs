5fe3d7b5317a64f66a1ae321ea48fdf7b27b8a71
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-09 13:15:05 +0200

    Break en continue in een for loop

--- for-loop/Program.cs
+++ for-loop/Program.cs
@@ -13,13 +13,36 @@ namespace for_loop
              * onder elkaar afbeeld.
              */
             string extraTekst = "Goed Hoor!";
+            ushort breakPoint = 3;
 
+            Console.WriteLine("Dit is een voorbeeld van het gebruik van break in een loop");
+            Console.WriteLine("-----------------------------------------------------------\n");
 
             for (ushort i = 0; i <= 10; i++)
             {
-                Console.WriteLine("De variabele i heeft de waarde: {0} {1}", i, extraTekst);
+                if (i != breakPoint)
+                {
+                    Console.WriteLine("De variabele i heeft de waarde: {0} {1}", i, extraTekst);
+                }
+                else
+                {
+                    Console.WriteLine("We hebben het getal gevonden namelijk: {0}", i);
+                    break;
+                }
             }
-            
+
+            Console.WriteLine("Dit is een voorbeeld van het gebruik van continue in een loop");
+            Console.WriteLine("-----------------------------------------------------------\n");
+
+            for (ushort i = 0; i <= 10; i++)
+            {
+                int evenGetal = i * 2;
+                if (evenGetal <= 10)
+                    Console.WriteLine("Dit is een even getal onder de tien {0}", evenGetal);
+                else
+                    break;
+            }
+
             Console.ReadLine();
         }
     }
