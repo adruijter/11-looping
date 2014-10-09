d43cbbe3de5a561c06156ec74b8f63bc7613f545
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-09 08:57:56 +0200

    Verbetering code do..while..loop.

--- do_while/Program.cs
+++ do_while/Program.cs
@@ -9,20 +9,29 @@ namespace do_while
     {
         static void Main(string[] args)
         {
-            /*  Voorbeeld van een do.. while lus
-             */
+            /*  Voorbeeld van een do.. while lus */
             ConsoleKeyInfo cki;
+            bool characterFound = false;           
+            
             Console.WriteLine("Voer een toetsenbord letter in!");
             Console.WriteLine("+++++++++++++++++++++++++++++++");
+
             do
             {
-                Console.WriteLine("\nU heeft niet de goede toets ingedrukt. \nProbeer nog eens!");
-                Console.Write("Nieuwe letter: ");
-                cki = Console.ReadKey();
-                Console.WriteLine("\n++++++++++++++++++++++++++++++++");
                 
+                if (characterFound)
+                Console.WriteLine("\nU heeft niet de goede toets ingedrukt. \nProbeer nog eens!");
+               
+                Console.Write("\nNieuwe letter: ");
+                cki = Console.ReadKey();
+                Console.WriteLine("\n++++++++++++++++");
 
-            } while( cki.KeyChar != (char)ConsoleKey.H);
+                // Als de invoer fout is (niet B) dan wordt characterFound true
+                characterFound = cki.KeyChar != (char)ConsoleKey.B;
+
+            } while(characterFound);
+
+
             Console.WriteLine("\nGefeliciteerd! \nU heeft de goede letter ingevoerd");
             Console.Write("De letter was: {0}", cki.KeyChar.ToString());
 
---
+++ for-loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Maak een for-loop die de getallen van 0 t/m 10
+             * onder elkaar afbeeld.
+             */
+            string extraTekst = "Goed Hoor!";
+
+
+            for (ushort i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("De variabele i heeft de waarde: {0} {1}", i, extraTekst);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
